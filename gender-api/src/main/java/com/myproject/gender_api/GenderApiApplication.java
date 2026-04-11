f2c755315a4e6baf8edf32f14e4d2c7af28bdc36
doomd <doomd4099@gmail.com>
2026-04-11 14:12:52 +0100

    chore: initial spring boot setup

---
+++ gender-api/src/main/java/com/myproject/gender_api/GenderApiApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.gender_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenderApiApplication.class, args);
+	}
+
+}
---
+++ gender-api/src/test/java/com/myproject/gender_api/GenderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.gender_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
